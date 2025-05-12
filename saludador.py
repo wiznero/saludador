3ab219b64e11d02007a21bb35ce4c1c399675940
sergio jimenez <C:\Users\nero9\AppData\Roaming\The Bat!>
2025-05-12 13:00:21 +0200

    ACtualiza version a 1.1.0 tras agregar funcionalidad de despedida

---
+++ saludador.py
@@ -0,0 +1,29 @@
+def saludo1():
+    print("Hola buenos dias encantado")
+
+def saludo2():
+    print("buenas tardes capullo")
+
+
+def saludo3():
+    print("hola caracopa")
+
+def despedirse():
+    print("me las piro vampiro")
+
+def despiderse2():
+    print("marcho que teño que marchar")
+
+
+
+opcion = input("introduces una opcion (1,2 o 3): ")
+
+if opcion == "1":
+    saludo1()
+    input("presione enter para continuar")
+elif opcion == "3":
+    saludo2()
+    input("presione enter para continuar")
+elif opcion == "3":
+    saludo2()
+    input("presione enter para continuar")
